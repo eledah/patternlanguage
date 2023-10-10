2bd5104bbbed59a681dade6c9ea7c45b0fc6f793
Eledah <eledah.niles@yahoo.com>
2023-10-10 16:31:20 +0330

    404 - Finalized

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,18 +4,18 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <>
-      <div className='w-4/5 mx-auto mt-20 flex flex-col justify-center items-center space-y-4'>
-        <h1 className='text-4xl font-semibold'>۴.۴ - این الگو هنوز اضافه نشده است</h1>
+      <div className='w-4/5 mx-auto flex flex-col justify-center items-center space-y-4'>
+        <h1 className='text-4xl font-semibold'>این الگو هنوز ترجمه نشده است</h1>
         <div className='space-x-4'>
           <Link
-            className='underline text-blue-600 hover:text-red-500 duration-300'
             href='/'
           >
             برگشت به صفحهٔ اول
           </Link>
+          &nbsp; | &nbsp;
           <Link
-            className='underline text-blue-600 hover:text-red-500 duration-300'
             href='https://github.com/eledah/patternlanguage/'
+            target='_blank'
           >
             مشارکت در تکمیل الگوها
           </Link>
